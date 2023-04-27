23b5e02ec069f9b2c348c64129344410406faddf
Minimata <minimata94@gmail.com>
2023-04-27 13:30:56 +0000

    building a hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="hero min-h-screen"
+    style="
+      background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);
+    "
+  >
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">{{ title }}</h1>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: ['Montserrat'],
+    },
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 
   daisyui: {
     themes: ['pastel'],
